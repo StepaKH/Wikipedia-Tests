63d61ee99f1737c4b57d291be407435b9d002648
Artv1d <arturovid2001@gmail.com>
2025-04-25 00:38:13 +0300

    Исправлены замечания по аллюру

--- utils/activity_helper.py
+++ utils/activity_helper.py
@@ -6,15 +6,12 @@ class ActivityHelper:
     @staticmethod
     def get_current_activity(driver):
         """Получить текущую активность"""
-        print(driver.current_activity)
         return driver.current_activity
 
     @staticmethod
     def switch_to_activity(driver, activity_name):
         """Переключиться на указанную активность"""
         if activity_name in Config.ACTIVITIES:
-            print(f"Запуск активности: {Config.ACTIVITIES[activity_name]}")
             driver.start_activity(Config.APP_PACKAGE, Config.ACTIVITIES[activity_name])
-            print("Активность успешно запущена.")
         else:
             raise ValueError(f"Активность '{activity_name}' не найдена в Config.ACTIVITIES")
