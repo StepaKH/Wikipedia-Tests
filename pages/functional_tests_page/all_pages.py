3e763d9fce3d3d74ac906fb933c581c7e484a9d8
Artv1d <arturovid2001@gmail.com>
2025-05-24 06:54:26 +0300

    Добавлена страница explore, протестированы search и вкладки

--- pages/functional_tests_page/all_pages.py
+++ pages/functional_tests_page/all_pages.py
@@ -1,8 +1,10 @@
 from pages.functional_tests_page.onboarding_pages.onboarding_page import OnboardingPage
 from pages.functional_tests_page.saved_pages.saved_page import SavedPage
+from pages.functional_tests_page.explore_pages.explore_page import ExplorePage
 
 class AllPages:
     def __init__(self, driver, logger=None):
         self.driver = driver
         self.onboarding = OnboardingPage(driver, logger=logger)
         self.saved = SavedPage(driver, logger=logger)
+        self.explore = ExplorePage(driver, logger=logger)
